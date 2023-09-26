15cd26a3af63c555d777366a19516e78e222d2df
SjdTl <131764102+SjdTl@users.noreply.github.com>
2023-09-26 15:54:26 +0200

    Add files via upload

---
+++ EE2C11 lab/Result/Result/VGS results/VGS calculations.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+runs = 7
+
+data = [[] for _ in range(runs)]
+V_GS = [[] for _ in range(runs)]
+I_D = [[] for _ in range(runs)]
+I_D_sqrt = [[] for _ in range(runs)]
+
+filename = "VGS calculations.txt"
+folder = os.path.dirname(os.path.abspath(__file__))
+filepath = os.path.join(folder, filename)
+
+run = -1
+with open(filepath, 'r') as file:
+    lines = file.readlines()
+    for line in lines:
+        if ("Step Information:" in line):
+            run += 1
+        else:
+            if (run != -1):
+                data[run].append((line.strip("\n")).split("\t"))
+
+run_amount=0
+for run in data:
+    for elements in run:
+        V_GS[run_amount].append(float(elements[0]))
+        I_D[run_amount].append(float(elements[1]))
+        I_D_sqrt[run_amount].append(float(elements[2]))
+    run_amount += 1
+
+
+# Plot
+fig, ax = plt.subplots()
+for i in range(0, runs):
+    ax.plot(V_GS[i], I_D_sqrt[i], label = 'Run ' + str(i+1))
+ax.set_title("Test")
+
+plt.legend()
+plt.show()
+
---
+++ EE2C11 lab/Result/Result/VGS results/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+# Plot
+# fig, ax = plt.subplots()
+# ax.plot(V_GS[0], I_sqrt[0], label = 'Run 1')
+# ax.plot(V_GS[1], I_sqrt[1], label = 'Run 2')
+# ax.set_title("Test")
+# plt.legend()
+# plt.show()
